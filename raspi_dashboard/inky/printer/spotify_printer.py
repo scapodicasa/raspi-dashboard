8b503ca2c7c45526a81c642dcdfd5b6a6a1047a6
Simone Capodicasa <simone.capo@gmail.com>
2020-11-08 11:11:48 +0100

    [printer] handling now squared spotify images

--- raspi_dashboard/inky/printer/spotify_printer.py
+++ raspi_dashboard/inky/printer/spotify_printer.py
@@ -22,88 +22,86 @@ class SpotifyPrinter(PrinterBase):
         logger.info(self._result)
 
     def print_display(self, inky_display):
-
         result = self._result
 
         img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 
-        album = result.current_playing.item.show if result.current_playing.item.show is not None else result.current_playing.item.album
-        album_images = [i for i in album.images if i.height == 64]
-        image_found = False
+        try:
+            album = result.current_playing.item.show if result.current_playing.item.show is not None else result.current_playing.item.album
+            album_images = [i for i in album.images if i.height == 64]
+            image_found = False
 
-        if len(album_images) > 0:
-            image_found = True
-            album_image = album_images[0]
+            if len(album_images) > 0:
+                image_found = True
+                album_image = album_images[0]
 
-            try:
-                response = requests.get(album_image.url)
+                try:
+                    response = requests.get(album_image.url)
 
-                if response.status_code == 200:
-                    cover = Image.open(BytesIO(response.content))
-                    cover.convert("1")
-                    cover = np.asarray(cover)
+                    if response.status_code == 200:
+                        cover = Image.open(BytesIO(response.content))
+                        cover.convert("1")
+                        cover = np.asarray(cover)
 
-                    bw = []
+                        bw = []
 
-                    for x in cover:
-                        row = []
-                        bw.append(row)
-                        for y in x:
-                            row.append(sum(y) / len(y))
-                else:
+                        for x in cover:
+                            row = []
+                            bw.append(row)
+                            for y in x:
+                                row.append(sum(y) / len(y))
+                    else:
+                        logger.info(
+                            f"Error downloading image from: {album_image.url}.")
+                except Exception as ex:
                     logger.info(
                         f"Error downloading image from: {album_image.url}.")
-            except Exception as ex:
-                logger.info(
-                    f"Error downloading image from: {album_image.url}.")
-                logger.debug(ex)
+                    logger.debug(ex)
 
-        else:
-            logger.info("Image with height of 64 px not found.")
+            else:
+                logger.info("Image with height of 64 px not found.")
 
-        if image_found:
-            a = inky_display.HEIGHT / 2
-            b = album_image.height / 2
-            c = 7 + album_image.width
+            if image_found:
+                a = inky_display.HEIGHT / 2
+                b = len(bw) / 2
+                c = 7 + len(bw[0])
 
-            cover_counter = 0
+            for y in range(0, inky_display.HEIGHT):
+                for x in range(0, inky_display.WIDTH):
+                    color = inky_display.BLACK
+                    if image_found:
+                        if y >= a - b and y < a + b:
+                            if x >= 7 and x < c:
+                                if bw[int(y - (a - b))][int(x - 7)] > 128:
+                                    color = inky_display.WHITE
 
-        for y in range(0, inky_display.HEIGHT):
-            for x in range(0, inky_display.WIDTH):
-                color = inky_display.BLACK
-                if image_found:
-                    if y >= a - b and y < a + b:
-                        if x >= 7 and x < c:
-                            row = int(cover_counter / 64)
-                            column = cover_counter % 64
-                            if bw[row][column] > 128:
-                                color = inky_display.WHITE
+                    img.putpixel((x, y), color)
 
-                            cover_counter = cover_counter + 1
+            intuitive_font = ImageFont.truetype(Intuitive, size=12)
 
-                img.putpixel((x, y), color)
+            opera = result.current_playing.item.show.name if result.current_playing.item.show is not None else result.current_playing.item.album.name
 
-        intuitive_font = ImageFont.truetype(Intuitive, size=12)
+            artist = result.current_playing.item.show.publisher if result.current_playing.item.show is not None else ",".join(
+                [a.name for a in result.current_playing.item.artists])
 
-        opera = result.current_playing.item.show.name if result.current_playing.item.show is not None else result.current_playing.item.album.name
-
-        artist = result.current_playing.item.show.publisher if result.current_playing.item.show is not None else ",".join(
-            [a.name for a in result.current_playing.item.artists])
-
-        msg = f"""T: {result.current_playing.item.name}
+            msg = f"""T: {result.current_playing.item.name}
 O: {opera}       
 A: {artist}"""
 
-        w, h = intuitive_font.getsize_multiline(msg)
-        w = 7 + (7 + album_image.width if image_found else 0)
-        h = int((inky_display.HEIGHT - h) / 2)
+            w, h = intuitive_font.getsize_multiline(msg)
+            w = 7 + (7 + album_image.width if image_found else 0)
+            h = int((inky_display.HEIGHT - h) / 2)
 
-        draw = ImageDraw.Draw(img)
+            draw = ImageDraw.Draw(img)
 
-        draw.multiline_text((w, h), msg, inky_display.WHITE,
-                            font=intuitive_font)
+            draw.multiline_text((w, h), msg, inky_display.WHITE,
+                                font=intuitive_font)
 
-        inky_display.set_border(inky_display.RED)
+            inky_display.set_border(inky_display.RED)
 
-        inky_display.set_image(img)
-        inky_display.show()
+            inky_display.set_image(img)
+            inky_display.show()
+
+        except Exception as ex:
+            logger.info("Unexpected exception happened.")
+            logger.debug(ex)
